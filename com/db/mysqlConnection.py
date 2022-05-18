61183ba8369e5f18a6c88357acc2e11291794208
李益洪 <ex_liyh33@partner.midea.com>
2022-05-18 12:28:26 +0800

    修改MySQL方法

--- com/db/mysqlConnection.py
+++ com/db/mysqlConnection.py
@@ -5,31 +5,64 @@
 @file    :mysqlConnection.py
 """
 import pymysql
+import logging
 
 
-class MySqlCom(object):
+class MySqlConnect(object):
     def __init__(self, host='localhost', user='root', password='muzili', database='ApiTool', port=3306, charset='utf8'):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
-        self.port = port
-        self.charset = charset
-        self.db = None
+        logging.debug("connect mysql: host={host}, port={port}, user={user}, password={password}, "
+                      "database={database}, charset={charset}".format(host=host, port=port, user=user,
+                                                                      password=password, database=database,
+                                                                      charset=charset))
+        try:
+            self.db = pymysql.Connect(host=host, port=port, user=user, password=password,
+                                      database=database, charset=charset)
+        except Exception as e:
+            logging.error("MySQL连接异常>>>{}".format(e))
 
     def __del__(self):
         try:
             self.db.close()
-        except AttributeError as e:
-            print(e)
+        except Exception as e:
+            logging.error("MySQL关闭异常>>>{}".format(e))
 
-    def to_connected(self):
+    def execute(self, sql):
+        """
+        新增、修改、删除
+        :param sql:
+        :return:
+        """
         try:
-            self.db = pymysql.Connect(host=self.host, port=self.port, user=self.user, password=self.password,
-                                      database=self.database, charset=self.charset)
-            return True
-        except pymysql.err.OperationalError:
-            return False
+            # 重连
+            self.db.ping(reconnect=True)
+            cur = self.db.cursor()
+            cur.execute(sql)
+            self.db.commit()
+            cur.close()
+        except Exception as e:
+            self.db.rollback()
+            logging.error("MySQL执行异常>>>{}".format(e))
+
+    def query(self, sql, is_dict=False):
+        """
+        查询
+        :param sql:
+        :param is_dict:
+        :return:
+        """
+        try:
+            # 重连
+            self.db.ping(reconnect=True)
+            if is_dict:
+                cur = self.db.cursor(cursor=pymysql.cursors.DictCursor)
+            else:
+                cur = self.db.cursor()
+            cur.execute(sql)
+            data = cur.fetchall()
+            cur.close()
+            return data
+        except Exception as e:
+            logging.error("MySQL执行异常>>>{}".format(e))
 
 
 if __name__ == "__main__":
