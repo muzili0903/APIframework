3f1d6f3d41ed67a0062edb9f6c2bab4890b89134
李益洪 <ex_liyh33@partner.midea.com>
2022-05-26 16:53:44 +0800

    新增checkData文件

---
+++ com/core/checkData.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  :ex_liyh33
+@time    :2022/5/26 12:24
+@file    :checkData.py
+"""
+from com.core import replaceData
+
+
+def check_req(response_body: dict, expect_body: dict):
+    pass
+
+
+def check_resp(response_body: dict, expect_body: dict) -> bool:
+    return True
+
+
+def check_db(response_body: dict, expect_body: dict) -> bool:
+    return True
+
+
+if __name__ == "__main__":
+    pass
--- com/core/checkResult.py
+++ com/core/checkResult.py
@@ -4,6 +4,7 @@
 @time    :2022/5/26 8:30
 @file    :checkResult.py
 """
+from com.core import checkData
 
 
 def check_res(response_body: dict, expect_body: dict):
@@ -13,7 +14,22 @@ def check_res(response_body: dict, expect_body: dict):
     :param expect_body: 预期结果
     :return:
     """
-    pass
+    result = list()
+    for key, value in expect_body.items():
+        if key.lower() == 'check_json':
+            result.append(checkData.check_resp(response_body, value))
+        elif key.lower() == 'check_db':
+            result.append(checkData.check_db(response_body, value))
+        elif key.lower() == 'check_part':
+            pass
+        elif key.lower() == '':
+            pass
+        else:
+            pass
+    if False not in result:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
@@ -24,4 +40,5 @@ if __name__ == "__main__":
             'expected_result': 'getAdultCurbactList_response.json'},
         'check_2': {'check_type': 'check_db'}}
     reponse_body = {'response_code': 200, 'response_body': {'code': '00000', 'msg': '操作成功'}}
+    print(check_res(reponse_body, expect_body))
     pass
