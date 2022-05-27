3e8f81ca18d2e87fd2b4c82b664fbdf4b699b701
李益洪 <ex_liyh33@partner.midea.com>
2022-05-27 18:01:20 +0800

    优化

--- com/core/checkData.py
+++ com/core/checkData.py
@@ -21,30 +21,37 @@ def check_list(response_body: list, expect_body: list, check_type) -> bool:
     :param check_type:
     :return:
     """
-    result = True
+    result = list()
     if check_type in ['perfect_match', '==']:
         if response_body.__len__() != expect_body.__len__():
-            result = False
-            return result
+            return False
     try:
         for index, value in enumerate(expect_body):
             if isinstance(value, dict):
-                result = check_resp(response_body[index], value, check_type)
+                # result = check_resp(response_body[index], value, check_type)
+                result.append(check_resp(response_body[index], value, check_type))
                 response_body.remove(value)
                 expect_body.remove(value)
             elif isinstance(value, list):
-                result = check_list(response_body[index], value, check_type)
+                # result = check_list(response_body[index], value, check_type)
+                result.append(check_list(response_body[index], value, check_type))
                 response_body.remove(value)
                 expect_body.remove(value)
             else:
+                if str(value) == str(response_body[index]):
+                    pass
+                else:
+                    return False
                 # if isinstance(value, str):
                 #     if str(value) == str(response_body[index]):
                 #         result = True
                 #     else:
                 #         result = False
-                if value in response_body:
-                    response_body.remove(value)
-                    expect_body.remove(value)
+                # if value in response_body:
+                #     response_body.remove(value)
+                #     expect_body.remove(value)
+                # else:
+                #     return False
                 # else:
                 #     if value == response_body:
                 #         response_body.remove(value)
@@ -54,16 +61,17 @@ def check_list(response_body: list, expect_body: list, check_type) -> bool:
         else:
             if check_type in ['perfect_match', '==']:
                 if response_body.__len__() != expect_body.__len__():
-                    result = False
-                    return result
+                    return False
                 else:
-                    result = True
-                    return result
+                    pass
     except Exception as e:
-        result = False
         logging.info("JSON格式校验，预期结果{}与响应结果{}值不一致".format(expect_body, response_body))
         logging.info("值校验报错{}".format(e))
-    return result
+        return False
+    if False not in result:
+        return True
+    else:
+        return False
 
 
 def check_resp(response_body: dict, expect_body: dict, check_type) -> bool:
@@ -76,30 +84,36 @@ def check_resp(response_body: dict, expect_body: dict, check_type) -> bool:
     check_type: perfect_match
     expected_result: test_response.json
     """
-    resp = False
+    result = list()
+    # resp = False
     if isinstance(expect_body, dict):
         for key, value in expect_body.items():
             if key not in response_body:
                 logging.info("JSON格式校验，关键字{}不在响应结果{}中".format(key, response_body))
-                return resp
+                return False
             else:
                 if isinstance(value, dict) and isinstance(response_body.get(key), dict):
-                    resp = check_resp(value, response_body.get(key), check_type)
+                    # resp = check_resp(value, response_body.get(key), check_type)
+                    result.append(check_resp(value, response_body.get(key), check_type))
                 elif not isinstance(value, type(response_body.get(key))):
                     logging.info("JSON格式校验，关键字{}预期结果{}与响应结果{}类型不符".format(key, value, response_body.get(key)))
-                    return resp
+                    return False
                 else:
                     if isinstance(value, list):
-                        resp = check_list(response_body.get(key), value, check_type)
+                        # resp = check_list(response_body.get(key), value, check_type)
+                        result.append(check_list(response_body.get(key), value, check_type))
                     else:
                         if str(value) == str(response_body.get(key)):
-                            resp = True
+                            pass
                         else:
-                            resp = False
+                            return False
     else:
         logging.info("JSON校验内容非dict格式：{}".format(expect_body))
-        return resp
-    return resp
+        return False
+    if False not in result:
+        return True
+    else:
+        return False
 
 
 def check_db(response_body: dict, expect_body: dict) -> bool:
@@ -110,9 +124,9 @@ if __name__ == "__main__":
     # test {'test': {"test": 1}} {'test': [1]]}}
     test = {'test': [{"test1": 2}, {"test": [3]}]}
     test1 = {'test': [{"test1": 2}, {"test": [33]}]}
-    test2 = {'test1': 3, 'test2': "22"}
-    test3 = {'test1': 33, 'test2': "22"}
+    test2 = {'test1': 123, 'test2': "22"}
+    test3 = {'test1': 1233, 'test2': "22"}
     # bug 待解决
     print(check_resp(test, test1, 'partial_match'))
-    print(check_resp(test2, test3, 'partial_match'))
+    # print(check_resp(test2, test3, 'partial_match'))
     pass
