871f2e1aefe3a24c3ba4ddf971db2bf524e3ca2c
李益洪 <ex_liyh33@partner.midea.com>
2022-06-23 09:29:58 +0800

    引入python多线程初探

--- testmutil/test_1.py
+++ testmutil/test_1.py
@@ -2,13 +2,15 @@
 """
 @Author  :ex_liyh33
 @time    :2022/6/1 16:56
-@file    :test.py
+@file    :test_1.py
 """
-from com.util.getConfig import Config
+import pytest
 
-con = Config()
-t = {'te':1}
-print(list(t.keys()))
+
+@pytest.mark.parametrize('a', [1, 2, 3])
+def test_1(a):
+    print(a)
+
 
 if __name__ == "__main__":
     pass
