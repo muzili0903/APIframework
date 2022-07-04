4f99e0c7c191310ac5b61c68d24ddb5915947be8
李益洪 <ex_liyh33@partner.midea.com>
2022-07-04 18:16:44 +0800

    dispose_int初探

--- com/left/fieldsDispose.py
+++ com/left/fieldsDispose.py
@@ -5,10 +5,84 @@
 @File ： fieldsDispose.py
 @IDE  ： PyCharm
 """
+import math
+
+from com.util.sysFunc import fnum
 
 
-def dispose_int():
-    pass
+def dispose_int(field: dict):
+    normal_value = list()
+    abnormality_value = list()
+    default = None
+    if field.get('isRequired') is None or field.get('isRequired').__eq__('N'):
+        # 字段正常案例校验
+        value = {
+            'title': '{} 字段为空'.format(field.get('field')),
+            field.get('field'): '',
+        }
+        normal_value.append(value)
+        value = {
+            'title': '{} 字段不传'.format(field.get('field')),
+            field.get('field'): None,
+        }
+        normal_value.append(value)
+        value = {
+            'title': '{} 字段输入正确的最大值'.format(field.get('field')),
+            field.get('field'): math.pow(10, field.get('length')) - 1,
+        }
+        normal_value.append(value)
+        value = {
+            'title': '{} 字段输入正确的最小值'.format(field.get('field')),
+            field.get('field'): 0,
+        }
+        normal_value.append(value)
+
+        # 字段异常案例校验
+        value = {
+            'title': '{} 字段输入超长的值'.format(field.get('field')),
+            field.get('field'): math.pow(10, field.get('length')),
+        }
+        abnormality_value.append(value)
+        value = {
+            'title': '{} 字段输入类型错误'.format(field.get('field')),
+            field.get('field'): fnum(int(field.get('length'))),
+        }
+        abnormality_value.append(value)
+    else:
+        # 字段正常案例校验
+        value = {
+            'title': '{} 字段输入正确的最大值'.format(field.get('field')),
+            field.get('field'): math.pow(10, field.get('length')) - 1,
+        }
+        normal_value.append(value)
+        value = {
+            'title': '{} 字段输入正确的最小值'.format(field.get('field')),
+            field.get('field'): 0,
+        }
+        normal_value.append(value)
+
+        # 字段异常案例校验
+        value = {
+            'title': '{} 字段输入超长的值'.format(field.get('field')),
+            field.get('field'): math.pow(10, field.get('length')),
+        }
+        abnormality_value.append(value)
+        value = {
+            'title': '{} 字段输入类型错误'.format(field.get('field')),
+            field.get('field'): fnum(int(field.get('length'))),
+        }
+        abnormality_value.append(value)
+        value = {
+            'title': '{} 字段为空'.format(field.get('field')),
+            field.get('field'): '',
+        }
+        abnormality_value.append(value)
+    if field.get('default') is not None:
+        default = {
+            'title': '{} 字段默认值'.format(field.get('field')),
+            field.get('field'): field.get('default'),
+        }
+    return {'normal_value': normal_value, 'abnormality_value': abnormality_value, 'default': default}
 
 
 def dispose_float():
@@ -28,4 +102,12 @@ def dispose_object():
 
 
 if __name__ == "__main__":
-    pass
+    field = {
+        'field': 'id',
+        'type': int,
+        'length': 8,
+        'default': 32,
+        'isRequired': 'Y'
+    }
+    fields = dispose_int(field)
+    print(fields)
