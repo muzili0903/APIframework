4d60bc5824ca7f7b0c1ff070077202623a309dfb
李益洪 <ex_liyh33@partner.midea.com>
2023-06-28 12:56:13 +0800

    1

---
+++ testCase/pro/apis/test_2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  :muzili
+@time    :2023/6/28 12:54
+@file    :test_2.py
+"""
+def test_1(login_and_logout):
+    assert 1 == 1
+
+def test_2(login_and_logout):
+    assert 1 == 1
+
+def test_3(login_and_logout):
+    assert 1 == 1
